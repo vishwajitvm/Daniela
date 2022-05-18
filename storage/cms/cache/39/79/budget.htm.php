4fcc2f06bfa2d555b7dcd899fddfdb6017195b06
vishwajitvm <vishwajitmall50@gmail.com>
2022-05-18 16:44:45 +0530

    second commit

---
+++ storage/cms/cache/39/79/budget.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms6284d4a064b8c573007490_2ccc56eb1ab4450bad657942c7d29854Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/twig/81/81865ffc628b13605f163b3625c294023df6f7a12009312ad090c80d62f80799.php
@@ -0,0 +1,126 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* C:\laravel\office\Daniela\themes\telassantoantonio\pages\budget.htm */
+class __TwigTemplate_2a5deb1a050d9483de9ecc56671a3759b9709f4d06be23cc61cf97954230359b extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"form\">
+    <div class=\"container\">
+        <div class=\"row\">
+            <div class=\"col-lg-8 offset-lg-2\">
+
+                <h1 class=\"form-title\">Qual é o seu projeto?</h1>
+                <h2 class=\"form-subtitle\">
+                    Descreva a sua necessidade para que um de nossos consultores possa melhor atendê-lo. <br>
+                    O seu projeto é muito importante para nós!
+                </h2>
+
+                ";
+        // line 12
+        $context['__cms_component_params'] = [];
+        echo $this->env->getExtension('Cms\Twig\Extension')->componentFunction("ContactForm"        , $context['__cms_component_params']        );
+        unset($context['__cms_component_params']);
+        // line 13
+        echo "
+            </div>
+        </div>
+    </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "C:\\laravel\\office\\Daniela\\themes\\telassantoantonio\\pages\\budget.htm";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  56 => 13,  52 => 12,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div class=\"form\">
+    <div class=\"container\">
+        <div class=\"row\">
+            <div class=\"col-lg-8 offset-lg-2\">
+
+                <h1 class=\"form-title\">Qual é o seu projeto?</h1>
+                <h2 class=\"form-subtitle\">
+                    Descreva a sua necessidade para que um de nossos consultores possa melhor atendê-lo. <br>
+                    O seu projeto é muito importante para nós!
+                </h2>
+
+                {% component 'ContactForm' %}
+
+            </div>
+        </div>
+    </div>
+</div>", "C:\\laravel\\office\\Daniela\\themes\\telassantoantonio\\pages\\budget.htm", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("component" => 12);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['component'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
